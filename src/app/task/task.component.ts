4365006fc01ce0fcf613cfccd7c4801322fc0f04
bhadresh gosai <gosai09@gmail.com>
2019-06-13 18:27:22 -0400

    done
    uploaded

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+name ="";
+date = "";
+assigned = "";
+  constructor() { }
+data = [
+	{"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
+	{"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
+	{"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
+	{"name": "Test Task #4", "date": "12/04/2012", "assigned": "John Doe" },
+	{"name": "Test Task #5", "date": "12/05/2012", "assigned": "John Doe" },
+	{"name": "Test Task #6", "date": "12/06/2012", "assigned": "John Doe" },
+	{"name": "Test Task #7", "date": "12/07/2012", "assigned": "John Doe" }
+];
+  ngOnInit() {
+  }
+  clickSave()
+  {
+    var obj = {"name":this.name,"date":this.date,"assigned":this.assigned};
+    this.data.push(obj);
+    console.log(JSON.stringify(this.data));
+  }
+  remove(id)
+  {
+    this.data.splice(id,1);
+  }
+
+}
+;
